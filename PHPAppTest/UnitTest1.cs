80882fa9028e167544a7aad07e26859f65fbac7e
Hackathon-User <sudipta.samanta.12.5@gmail.com>
2019-02-22 14:18:31 +0530

    Testing commit

---
+++ PHPAppTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace PHPAppTest 
+{
+   [TestFixture]
+    public class UnitTest1
+    {
+        
+        [TestCase]
+        public void TestMethod1()
+        {
+
+            int a = 20;
+            int b = 10;
+            int c = a + b;
+            Assert.AreEqual(30, c);
+        }
+    }
+}
